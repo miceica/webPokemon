66294cdb27dc1fea892de7d6beb1a8bff54fb215
miceica <miguelceica23@gmail.com>
2024-03-11 20:38:47 +0100

    ultimos cambios

--- js/index.js
+++ js/index.js
@@ -1,10 +1,30 @@
 window.onload = () => {
     let menu = document.getElementById("barras-menu");
     menu.onclick = () => {
-        if(document.getElementById("menu-movil").classList.contains("menu-movil")){
+        if (document.getElementById("menu-movil").classList.contains("menu-movil")) {
             document.getElementById("menu-movil").classList.remove("menu-movil");
-        }else{
+        } else {
             document.getElementById("menu-movil").classList.add("menu-movil");
         }
     }
+
+    let url = "https://pokeapi.co/api/v2/pokemon/";
+
+    //mostramos loading
+    document.getElementById("loading").style.display="block";
+
+    fetch(url)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error(`Network response was not ok`);
+            }
+            return response.json();
+        })
+        .then(data => {
+            document.getElementById("loading").style.display="none";
+            console.log(data);
+        })
+        .catch(error => {
+            console.error('Error during fetch operation:', error);
+        });
 }
